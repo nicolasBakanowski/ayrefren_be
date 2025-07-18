14a640241f81c78b107e00cb4934b49a16563d98
Marcos Olmedo <62622400+marwos-dev@users.noreply.github.com>
2025-07-18 02:28:13 -0300

    Fix init script import path

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-python scripts/wait_for_db.py
+python -m scripts.wait_for_db
 alembic upgrade head
-python scripts/init_db.py
+python -m scripts.init_db
 
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000 "$@"
