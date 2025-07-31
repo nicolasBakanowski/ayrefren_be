5ae0d8cf943d017534a2b967c33d912428315a9d
NIcolas Bakanowski <63619842+nicolasBakanowski@users.noreply.github.com>
2025-07-30 22:53:00 -0300

    Fix migration head and defaults

--- alembic/versions/c45d6741ac72_add_invoice_accepted_column.py
+++ alembic/versions/c45d6741ac72_add_invoice_accepted_column.py
@@ -1,7 +1,7 @@
 """add invoice accepted column
 
 Revision ID: c45d6741ac72
-Revises: f5e1b8174ed1
+Revises: b32dd8c9c367
 Create Date: 2025-07-31 01:40:00
 """
 from typing import Sequence, Union
@@ -10,14 +10,22 @@ from alembic import op
 import sqlalchemy as sa
 
 revision: str = 'c45d6741ac72'
-down_revision: Union[str, Sequence[str], None] = 'f5e1b8174ed1'
+down_revision: Union[str, Sequence[str], None] = 'b32dd8c9c367'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.add_column('invoices', sa.Column('accepted', sa.Boolean(), nullable=True))
+    op.add_column(
+        "invoices",
+        sa.Column(
+            "accepted",
+            sa.Boolean(),
+            nullable=False,
+            server_default=sa.text("false"),
+        ),
+    )
 
 
 def downgrade() -> None:
